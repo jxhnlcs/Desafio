46da19012fad31b96737e22eb90bfec2766cd067
John Lucas <106848240+jxhnlcs@users.noreply.github.com>
2023-11-26 21:21:20 -0300

    Update historic.component.ts

--- web/src/app/pages/historic/historic.component.ts
+++ web/src/app/pages/historic/historic.component.ts
@@ -18,22 +18,19 @@ export class HistoricComponent implements OnInit {
   formatDateTime(dateTime: string): string {
     const date = new Date(dateTime);
     
-    // Obtém os componentes de data e hora
     const day = date.getDate();
-    const month = date.getMonth() + 1; // Mês é baseado em zero, então adicionamos 1
+    const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const hours = date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds();
 
-    // Formatação com zeros à esquerda, se necessário
     const formattedDay = day < 10 ? `0${day}` : `${day}`;
     const formattedMonth = month < 10 ? `0${month}` : `${month}`;
     const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
     const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
     const formattedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
 
-    // Retorna a data e hora formatadas
     return `${formattedDay}/${formattedMonth}/${year} ${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   }
 
